39bcaca4a891fcfa6e4a5932033756dc9a3b73e4
Alexandr Trushnikov <alexandr.trushnikov@spb-atrushnikov.team.wrike.com>
2020-01-10 22:50:42 +0300

    start_urls getting from redis for a rutopic spider

--- rutracker/rutracker/spiders/rutopic.py
+++ rutracker/rutracker/spiders/rutopic.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import redis
+import json
 
 r = redis.Redis('192.168.64.6')
 
@@ -8,7 +9,9 @@ r = redis.Redis('192.168.64.6')
 class RutopicSpider(scrapy.Spider):
     name = 'rutopic'
     allowed_domains = ['rutracker.org']
-    start_urls = ['http://rutracker.org/']
+    start_urls = [
+        json.loads(r.get(x).decode())['url'] for x in r.keys('rutracker_*')
+    ]
 
     def parse(self, response):
         pass
